86d65f8ebfdf83c41b343ab0e99d5d72a45eeff8
nithishbarath-r <rnithishbarath@gmail.com>
2026-06-19 16:12:39 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("ChatBot Started")
+    print("Type 'bye' to exit.")
+
+    while True:
+        user = input("You: ").lower()
+
+        if user == "hello":
+            print("Bot: Hi!")
+        elif user == "how are you":
+            print("Bot: I'm fine, thanks!")
+        elif user == "what is your name":
+            print("Bot: I am CodeAlpha Bot.")
+        elif user == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: Sorry, I don't understand.")
+
+chatbot()
